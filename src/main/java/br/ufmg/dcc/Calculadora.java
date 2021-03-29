ea0167e60c078c84cec5655bd0a24e6af090a741
lcharles123 <root@master>
2021-03-29 16:03:43 -0300

    colocado int grange

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -3,7 +3,7 @@ package br.ufmg.dcc;
 public class Calculadora {
 
   public int soma(int x, int y) {
-    return x + y*9;
+    return x + y*999999999999999999999999999999;
   }
 
   public int subtrai(int x, int y) {
