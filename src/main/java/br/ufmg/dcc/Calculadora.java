f6a53fd4d72d6f5278ae48b97f0b63790efc29e2
pedro.belo <pedrohenrique1550@hotmail.com>
2023-02-23 20:19:20 -0300

    Incluindo alterações na função soma

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -3,11 +3,11 @@ package br.ufmg.dcc;
 public class Calculadora {
 
   public int soma(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtrai(int x, int y) {
     return x - y;
   }
 
-}
+} 
