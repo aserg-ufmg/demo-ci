560502a28e5af65b6c6c001a6d56ebd681066fe5
Neumar <neumar@ufla.br>
2023-10-23 14:00:18 -0300

    adicioina comentario na funcao soma

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -3,7 +3,8 @@ package br.ufmg.dcc;
 public class Calculadora {
 
   public int soma(int x, int y) {
-    return x + y;
+    // esta funcao calcula a soma de 2 inteiros
+    return x + 2*y;
   }
 
   public int subtrai(int x, int y) {
