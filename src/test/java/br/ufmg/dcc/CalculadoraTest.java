6c8a5649d994b445473792fe53b664374c945759
Rhuan Mateus <145929929+RhuanMateus@users.noreply.github.com>
2023-11-20 14:31:57 -0300

    Update CalculadoraTest.java

--- src/test/java/br/ufmg/dcc/CalculadoraTest.java
+++ src/test/java/br/ufmg/dcc/CalculadoraTest.java
@@ -13,7 +13,7 @@ public class CalculadoraTest {
   }
 
   @Test
-  public void testeSoma() {
+  public void testeSoma3() {
     Calculadora calc = new Calculadora();
     assertEquals(10, calc.soma(4,6));
   }
